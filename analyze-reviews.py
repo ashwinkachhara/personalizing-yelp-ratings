4f2d735b482e165c9a155e646ad1a9895c90c534
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2015-11-22 13:19:02 -0500

    init

---
+++ analyze-reviews.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 13:18:25 2015
+
+@author: ashwin
+"""
+
---
+++ testdata-extraction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 13:16:51 2015
+
+@author: ashwin
+"""
+

47f5e12233ca669878032334b01604e7736162a8
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2015-11-22 16:25:06 -0500

    Data for IL extracted, and properly indexed

--- analyze-reviews.py
+++ analyze-reviews.py
@@ -5,3 +5,20 @@ Created on Sun Nov 22 13:18:25 2015
 @author: ashwin
 """
 
+import pickle
+
+pklfile = open('reviews.pkl','rb')
+reviews = pickle.load(pklfile)
+pklfile.close()
+
+pklfile = open('restaurants.pkl','rb')
+restaurants = pickle.load(pklfile)
+pklfile.close()
+
+pklfile = open('reviews_user.pkl','rb')
+reviews_user = pickle.load(pklfile)
+pklfile.close()
+
+pklfile = open('users.pkl','rb')
+users = pickle.load(pklfile)
+pklfile.close()
--- testdata-extraction.py
+++ testdata-extraction.py
@@ -4,4 +4,115 @@ Created on Sun Nov 22 13:16:51 2015
 
 @author: ashwin
 """
+import json
+import pandas as pd
+from operator import itemgetter
+from sets import Set
+import pickle
 
+
+def convert(ob):
+    for k, v in ob.items():
+        if isinstance(v, list):
+            ob[k] = ','.join(v)
+        elif isinstance(v, dict):
+            for kk, vv in v.items():
+                ob['%s_%s' % (k, kk)] = vv
+            del ob[k]
+    return ob
+
+filePrefix = "yelp_academic_dataset_"
+
+interest_categories = ["Restaurants", "Food"] # "Nightlife"
+interest_state = ['NV', 'AZ', 'PA', 'IL', 'WI', 'NC']
+state_index = 3 # We chose IL as test dataset because it had 334 businesses. Next was WI - 1249
+
+restaurants = []
+reviews = {}
+users = []
+reviews_user = {}
+
+businessesFile = open('data/'+ filePrefix + 'business.json')
+reviewsFile = open('data/'+ filePrefix + 'review.json')
+usersFile = open('data/'+ filePrefix + 'user.json')
+
+for line in businessesFile:
+    ob = json.loads(line)
+    if ob['state'] == interest_state[state_index] and len(list(set(interest_categories) & set(ob['categories']))) > 0:
+        restaurants.append(ob)
+
+print "Loaded",len(restaurants),"restaurants in",interest_state[state_index]
+
+biz_ids = [d['business_id'] for d in restaurants]
+#print biz_ids[:10]
+
+for i in range(len(restaurants)):
+    reviews[biz_ids[i]] = []
+
+user_ids = Set([])
+#print filter(lambda biz: biz['business_id'] == biz_ids[0], restaurants)
+
+numReviews = 0
+for line in reviewsFile:
+    ob = json.loads(line)
+    if ob['business_id'] in biz_ids:
+        reviews[ob['business_id']].append(ob)
+        user_ids.add(ob['user_id'])
+        numReviews = numReviews + 1
+
+print "Found",numReviews,"reviews"
+        
+user_ids = list(user_ids)
+
+for line in usersFile:
+    ob = json.loads(line)
+    if ob['user_id'] in user_ids and ob['review_count'] >= 5:
+        users.append(ob)
+        
+print "Found", len(users), "users"
+
+suff_users = [u['user_id'] for u in users]
+
+for i in suff_users:
+    reviews_user[i] = []
+
+
+for biz_id,biz in reviews.iteritems():
+    for review in biz:
+        if review['user_id'] in suff_users:
+#            print review['user_id']
+            reviews_user[review['user_id']].append(review)
+            
+pklfile = open('reviews.pkl','wb')
+pickle.dump(reviews,pklfile)
+pklfile.close()
+
+pklfile = open('restaurants.pkl','wb')
+pickle.dump(restaurants,pklfile)
+pklfile.close()
+
+pklfile = open('reviews_user.pkl','wb')
+pickle.dump(reviews_user,pklfile)
+pklfile.close()
+
+pklfile = open('users.pkl','wb')
+pickle.dump(users,pklfile)
+pklfile.close()
+
+
+
+
+            
+#reviews_user = dict((k, v) for k, v in reviews_user.iteritems() if len(v)>0)
+#for user_id,review in reviews_user.iteritems():
+#    if len(review) > 0:
+##        print review
+
+
+#print reviews[biz_ids[0]]
+    
+    
+#df = pd.DataFrame(restaurants)
+
+#print df
+    

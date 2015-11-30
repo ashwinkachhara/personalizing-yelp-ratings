3dadc59190b93dc6522dace57c682bd1c22fb471
Tyler Hastings <tyler@neadwerx.com>
2015-11-29 20:13:05 -0500

    generating a file where each review has a list of sentiments for each sentence.

--- perform-analysis.py
+++ perform-analysis.py
@@ -3,13 +3,36 @@
 # Compare words in sentence to words in bucket. If word matched perform a sentiment analysis.
 # Possible algorithm
 # wordMatches * sentiment  summed over all sentences
-import json, sys, os
+import json
+import pickle
+import requests
 
-from random import randrange
 from nltk.tokenize import sent_tokenize
 
-from sentiment.info import setup, MyDict, classify2, classify_demo
+# Import the old sentiment analyzer -- will replace.
+from sentiment.info import setup, MyDict, classify2
 setup()
+
+# Get a set of words for all of the grouping buckets
+pklfile = open('full-buckets.pkl', 'rb')
+tasteBucket, healthBucket, speedBucket = pickle.load(pklfile)
+pklfile.close()
+
+#remove \r if the words have it appended -- windows only
+for index in range(len(tasteBucket)):
+    tasteBucket[index] = tasteBucket[index].replace('\r', '')
+
+for index in range(len(healthBucket)):
+    healthBucket[index] = healthBucket[index].replace('\r', '')
+
+for index in range(len(speedBucket)):
+    speedBucket[index] = speedBucket[index].replace('\r', '')
+
+tasteBucket = set(tasteBucket)
+healthBucket = set(healthBucket)
+speedBucket = set(speedBucket)
+
+speedBucket.remove("pokey")
 
 # Create Flavor Class to store values
 class FlavorRating:
@@ -26,91 +49,111 @@ def get_sentiment(text):
     # print sentiment
 
     if sentiment[0] and (sentiment[1] > .5):
-        return 'pos'
+        return ['pos', sentiment[1]]
 
     if not sentiment[0] and (sentiment[1] > .5):
-        return 'neg'
-    return 'net'
+        return ['neg', sentiment[1]]
+
+    return ['neutral', 0]
+
+
+# Convert to this when finalizing and have data writing to file properly. -- should only use this on 1 run
+def get_sentiment_two(text2):
+    r = requests.post('http://text-processing.com/api/sentiment/', data="text=" + text2)
+    sentiment_json = json.loads(r.text)
+    return [sentiment_json['label'], sentiment_json['probability']['neg'], sentiment_json['probability']['neutral'], sentiment_json['probability']['pos'] ]
 
 
 def taste_algorithm():
-    return .2
+    return 1
 
-
-# Get a set of words for all of the grouping buckets
-tasteBucket = {"goldberg", "test1", "test2"}
-healthBucket = {"test", "general", "test2"}
-speedBucket = {"trying", "have", "test2"}
 
 # Load the file containing all of the reviews
 # NOTE: TEMP FILE FOR TESTING
-reviewsFile = open('data/yelp_academic_dataset_review.json')
+pklfile = open('reviews.pkl', 'rb')
+reviewsFile = pickle.load(pklfile)
+pklfile.close()
 
 # Create arrays to hold flavor values, we will average after processing all reviews
-healthScoreArray = []
-speedScoreArray = []
-tasteScoreArray = []
+# healthScoreArray = []
+# speedScoreArray = []
+# tasteScoreArray = []
 
-index = 0;
-# Loop through all of the reviews -- should either be for a user OR a business
-for line in reviewsFile:
-    index += 1
-    # Get the review json
-    reviewJson = json.loads(line)
 
-    # Get the review text
-    review = reviewJson['text']
+index = 0
+# Loop through all of the reviews -- Currently all of the reviews for the dataset we pulled
+for businesses in reviewsFile.values():
+    for i in range(len(businesses)):
+        index += 1
 
-    # Tokenize each of the sentences from the review
-    reviewSentences = sent_tokenize(review)
+        # Get the review json
+        reviewItem = businesses[i]
+        # reviewJson = json.loads(line)
 
-    # Loop through all of the sentences
-    for sentence in reviewSentences:
-        # Lowercase the sentence for matching
-        sentence = sentence.lower()
+        # Get the review text
+        review = reviewItem['text']
 
-        # Get the set of words in the sentence
-        sentenceWordSet = set(sentence.split())
+        # Tokenize each of the sentences from the review
+        reviewSentences = sent_tokenize(review)
 
-        # Check to see if the sentence contains any of the key-words
-        hasTaste = True if tasteBucket & sentenceWordSet else False
-        hasSpeed = True if speedBucket & sentenceWordSet else False
-        hashealth = True if healthBucket & sentenceWordSet else False
+        # Add the sentiment array to the current reviewItem
+        reviewItem['sentiment'] = []
 
-        # Get which grouping the sentence falls under
-        sentimentGroup = get_sentiment(sentence)
+        # Loop through all of the sentences
+        for sentence in reviewSentences:
+            # Lowercase the sentence for matching
+            sentence = sentence.lower()
 
-        # If neutral, skip this sentence
-        if sentimentGroup == 'net':
-            break
+            # Get the set of words in the sentence
+            sentenceWordSet = set(sentence.split())
 
-        print sentence
-        print sentimentGroup
+            # Check to see if the sentence contains any of the key-words
+            hasTaste = True if tasteBucket & sentenceWordSet else False
+            hasSpeed = True if speedBucket & sentenceWordSet else False
+            hasHealth = True if healthBucket & sentenceWordSet else False
 
-        # Try all of the buckets that matched
-        if hasTaste:
-            tasteScoreArray.append(-1 * taste_algorithm() if sentimentGroup == 'neg' else taste_algorithm())
+            # Get which grouping the sentence falls under
+            sentimentGroup = get_sentiment_two(sentence)
 
-        if hasSpeed:
-            speedScoreArray.append(-1 * taste_algorithm() if sentimentGroup == 'neg' else taste_algorithm())
+            # Add the sentences sentiment value to the array of sentiments
+            reviewItem['sentiment'].append(sentimentGroup)
 
-        if hashealth:
-            healthScoreArray.append(-1 * taste_algorithm() if sentimentGroup == 'neg' else taste_algorithm())
+            # If neutral, skip this sentence
+            if sentimentGroup[0] == 'neutral':
+                break
 
-    if index > 50:
-        break
+            # Try all of the buckets that matched
+            # if hasTaste:
+            #     # print sentence
+            #     # print 't:' + sentimentGroup
+            #     tasteScoreArray.append(-1 * taste_algorithm() if sentimentGroup[0] == 'neg' else taste_algorithm())
+            #
+            # if hasSpeed:
+            #     # print sentence
+            #     # print 's:' + sentimentGroup
+            #     speedScoreArray.append(-1 * taste_algorithm() if sentimentGroup[0] == 'neg' else taste_algorithm())
+            #
+            # if hashealth:
+            #     # print sentence
+            #     # print 'h:' + sentimentGroup
+            #     healthScoreArray.append(-1 * taste_algorithm() if sentimentGroup[0] == 'neg' else taste_algorithm())
+
 
 # Generate an overall flavorRating
-flavorRating = FlavorRating()
+#flavorRating = FlavorRating()
 
 # Set the flavor rating values
-flavorRating.health = sum(healthScoreArray) / float(len(healthScoreArray)) if len(healthScoreArray) > 0 else 0
-flavorRating.taste = sum(tasteScoreArray) / float(len(tasteScoreArray)) if len(tasteScoreArray) > 0 else 0
-flavorRating.speed = sum(speedScoreArray) / float(len(speedScoreArray)) if len(speedScoreArray) > 0 else 0
+#flavorRating.health = sum(healthScoreArray) / float(len(healthScoreArray)) if len(healthScoreArray) > 0 else 0
+#flavorRating.taste = sum(tasteScoreArray) / float(len(tasteScoreArray)) if len(tasteScoreArray) > 0 else 0
+#flavorRating.speed = sum(speedScoreArray) / float(len(speedScoreArray)) if len(speedScoreArray) > 0 else 0
 
-print flavorRating.taste
-print flavorRating.health
-print flavorRating.speed
+#print flavorRating.taste
+#print flavorRating.health
+#print flavorRating.speed
+
+pklfile = open('businesses_with_sentiment.pkl', 'wb')
+pickle.dump(reviewsFile, pklfile)
+pklfile.close()
 
 # http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
 # GET word frequency
